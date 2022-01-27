7d2cdf91ee349d11da57498e68222559e12b8e19
marccatalan <marc.catalan-sanchez@epitech.eu>
2022-01-27 10:53:28 +0100

    perfect

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** print example
+** File description:
+** example
+*/
+
+ #include "./lib/my/my.h"
+ #include "./include/include"
+
+ int main()
+ {
+     my_putstr("Hola");
+     return 0;
+ }
