b3146c5155fabe3055332bc8091b28ee44606092
marccatalan <marc.catalan-sanchez@epitech.eu>
2022-01-28 15:43:40 +0100

    adding env

--- main.c
+++ main.c
@@ -7,9 +7,15 @@
 
  #include "./lib/my/my.h"
  #include "./include/include"
+ #include <stdio.h>
 
- int main()
- {
-     my_putstr("Hola");
-     return 0;
- }
+
+int main(int argc, char **argv, char **envp)
+{
+  for (char **env = envp; *env != 0; env++) {
+    char *thisEnv = *env;
+    printf("%s\n", thisEnv);
+  }
+
+  return 0;
+}
